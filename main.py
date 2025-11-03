62f3f7c69728a4e2e72604ab9c1dbf9eae2597f3
yuuzuchi <tangd46@gmail.com>
2025-11-03 06:36:02 -0700

    view-source logic -> load(), refactor request()

--- main.py
+++ main.py
@@ -57,7 +57,7 @@ class URL:
         if self.scheme == "file":
             try:
                 with open(self.path, 'r') as file:
-                    content = file.read()
+                    return file.read()
             except FileNotFoundError:
                 return f"Error: The file '{self.path}' was not found."
             except Exception as e:
@@ -70,73 +70,68 @@ class URL:
                 return base64b.decode("utf8" if self.charset.lower() == "UTF-8" else "ascii")
             return self.data
         
-        else:
-            if not self.s:
-                self.s = socket.socket(
-                    family=socket.AF_INET,
-                    type=socket.SOCK_STREAM,
-                    proto=socket.IPPROTO_TCP
-                ) 
-                
-                if self.scheme == "https":
-                    ctx = ssl.create_default_context()
-                    self.s = ctx.wrap_socket(self.s, server_hostname=self.host)
-
-                self.s.connect((self.host, self.port))
-
-            request =  f"GET {self.path} HTTP/1.1\r\n"
-            request += f"Host: {self.host}\r\n"
-            request += f"Connection: keep-alive\r\n"
-            for header, value in headers.items():
-                request += f"{header}: {value}\r\n"
-            request += "\r\n"
-
-            self.s.send(request.encode("utf8"))
-
-            response = self.s.makefile("rb", encoding="utf8", newline="\r\n")
-            statusline = response.readline().decode("utf-8")
-            version, status, explanation = statusline.split(" ", 2)
-            
-            response_headers = {}
-            while True:
-                line = response.readline().decode("utf-8")
-                if line == "\r\n": 
-                    break
-                header, value = line.split(":", 1)
-                response_headers[header.casefold()] = value.strip()
-            
-            # handle redirects
-            if status.startswith('3') and "location" in response_headers:
-                content = self._redirect(response_headers["location"])
-            else:
-                # unchunk the data
-                content = bytearray()
-                if response_headers.get("transfer-encoding") == "chunked":
-                    while True:
-                        chunk_size = int(response.readline().decode("utf-8").strip(), 16)
-                        if chunk_size == 0:
-                            break
-                        chunk = response.read(chunk_size)
-                        content.extend(chunk)
-                        response.read(2)
-                
-                elif "content-length" in response_headers:
-                    content = response.read(int(response_headers["content-length"]))
-                    
-                # decompress and decode 
-                if response_headers.get("content-encoding") == "gzip":
-                    content = gzip.decompress(content).decode("utf-8")
-                else:
-                    content = content.decode("utf-8")
-                
-            self.redirects = 0
+        # http and https
         
-        if self.source:
-            return content.replace("<", "&lt;").replace(">", "&gt;")
-        return content
-    
+        if not self.s:
+            self.s = socket.socket(
+                family=socket.AF_INET,
+                type=socket.SOCK_STREAM,
+                proto=socket.IPPROTO_TCP
+            ) 
+            if self.scheme == "https":
+                ctx = ssl.create_default_context()
+                self.s = ctx.wrap_socket(self.s, server_hostname=self.host)
+                
+            self.s.connect((self.host, self.port))
+
+        request =  f"GET {self.path} HTTP/1.1\r\n"
+        request += f"Host: {self.host}\r\n"
+        request += f"Connection: keep-alive\r\n"
+        for header, value in headers.items():
+            request += f"{header}: {value}\r\n"
+        request += "\r\n"
+
+        self.s.send(request.encode("utf8"))
+        response = self.s.makefile("rb", encoding="utf8", newline="\r\n")
+        statusline = response.readline().decode("utf-8")
+        version, status, explanation = statusline.split(" ", 2)
+        
+        response_headers = {}
+        while True:
+            line = response.readline().decode("utf-8")
+            if line == "\r\n": 
+                break
+            header, value = line.split(":", 1)
+            response_headers[header.casefold()] = value.strip()
+        
+        # handle redirects
+        if status.startswith('3') and "location" in response_headers:
+            content = self._redirect(response_headers["location"])
+            self.redirects = 0
+            return content
+        
+        # unchunk the data
+        content = bytearray()
+        if response_headers.get("transfer-encoding") == "chunked":
+            while True:
+                chunk_size = int(response.readline().decode("utf-8").strip(), 16)
+                if chunk_size == 0:
+                    break
+                chunk = response.read(chunk_size)
+                content.extend(chunk)
+                response.read(2)
+        
+        # content-length cannot appear if chunked
+        elif "content-length" in response_headers:
+            content = response.read(int(response_headers["content-length"]))
+            
+        # decompress and decode 
+        if response_headers.get("content-encoding") == "gzip":
+            return  gzip.decompress(content).decode("utf-8")
+        return content.decode("utf-8")
+                
     def _redirect(self, location: str) -> str:
-        if self.redirects >= 100:
+        if self.redirects >= 20:
             return "Error: Redirect Limit Reached"
 
         if location.startswith("/"):
@@ -164,12 +159,15 @@ def show(body: str) -> None: # print all text between tags
             print(c, end='')
         i += 1
             
-def load(url: str) -> None:
+def load(url: URL) -> None:
     headers = {
         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36",
         "Accept-Encoding": "gzip"
     }
-    show(url.request(headers))
+    if url.source:
+        print(url.request(headers))
+    else:
+        show(url.request(headers))
     
 if __name__ == "__main__":
     import sys
